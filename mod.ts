bc1058f6575dea1f1d7a10468d7b157e385c4ee5
Steve_Zhang <wentaozhang0305@gmail.com>
2020-10-25 22:58:05 -0700

    fetched data from spacex

--- mod.ts
+++ mod.ts
@@ -1,10 +1,33 @@
-async function downloadLaunchData() {
-  const response = await fetch("https://api.spacexdata.com/v3/launches", {
-    method: "GET",
-  });
+// async function downloadLaunchData() {
+//   const response = await fetch("https://api.spacexdata.com/v3/launches", {
+//     method: "GET",
+//   });
 
-  const launchData = await response.json();
-  console.log(launchData);
+//   const launchData = await response.json();
+//   console.log(launchData);
+// }
+
+// await downloadLaunchData();
+
+// const res = await fetch("https://reqres.in/api/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8",
+//   },
+//   body: JSON.stringify({
+//     name: "Elon Musk",
+//     job: "billionaire",
+//   }),
+// });
+
+// const body = await res.json();
+// console.log(body);
+
+import * as log from "https://deno.land/std/log/mod.ts";
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
 }
 
-await downloadLaunchData();
+const launches = newMap<number, Launch>();
